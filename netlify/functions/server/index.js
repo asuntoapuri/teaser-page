89dd468a6b7396deba524c490ba92a11e7ba7d0a
lassidevs <lassi.koivisto7@gmail.com>
2024-12-07 10:04:19 +0200

    fix: tryfix

--- netlify/functions/server/index.js
+++
@@ -1,7 +0,0 @@
-import { createRequestHandler } from "@remix-run/netlify";
-import * as build from "./build.js";
-
-export const handler = createRequestHandler({
-  build,
-  mode: "production",
-});
--- remix.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  serverBuildTarget: "netlify",
-  server: "./server.js",
-  ignoredRouteFiles: ["**/.*"],
-  serverBuildPath: "netlify/functions/server/build.js", // This is important
-};
